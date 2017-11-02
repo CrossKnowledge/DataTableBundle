ce836e97da54ea31908455e4e5bd00ae98032215
Cyrielle <cyrielleC@users.noreply.github.com>
2017-11-02 15:53:56 +0100

    Update TwigRenderer.php

--- DataTable/Renderer/TwigRenderer.php
+++ DataTable/Renderer/TwigRenderer.php
@@ -15,15 +15,15 @@ class TwigRenderer implements RendererInterface
 
     public function render(AbstractTable $table)
     {
-        $template = $this->twig->loadTemplate($table->getOptions()['template']);
+        $tableOptions = $table->getOptions();
         $data = [
             'columns' => $table->getClientSideColumns(),
             'datatable' => $table,
         ];
-        if ($table->getOptions()['has_filter_form'])
+        if ($tableOptions['has_filter_form'])
         {
             $data['filterForm'] = $table->getFilterForm()->createView();
         }
-        return $template->render($data);
+        return $this->twig->loadTemplate($tableOptions['template'])->render($data);
     }
 }
