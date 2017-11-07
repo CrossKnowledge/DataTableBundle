321575d4cc5e7532be67be8609179d8ab1a85466
cchauvin <cyrielle.chauvin@gmail.com>
2017-11-07 14:11:36 +0100

    test

--- DataTable/Table/AbstractTable.php
+++ DataTable/Table/AbstractTable.php
@@ -128,6 +128,7 @@ abstract class AbstractTable
             'layout' => $this->layout,
             'client_side_filtering' => false,
             'filter_reload_table_on_change' => false,
+            'no_init_loading' => false,
             'template' => 'CrossKnowledgeDataTableBundle::default_table.html.twig',
             'data_table_custom_options' => [],
             'has_filter_form' => function() {
