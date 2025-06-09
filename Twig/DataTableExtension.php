23ad6299653429b0a2a9f683f5c017bde2fed908
NikitaRynkevich <mikita.rynkevich@innowise.com>
2025-06-09 15:31:49 +0200

    Hotfix to return type

--- Twig/DataTableExtension.php
+++ Twig/DataTableExtension.php
@@ -19,7 +19,7 @@ class DataTableExtension extends AbstractExtension
     /**
      * {@inheritdoc}
      */
-    public function getFunctions()
+    public function getFunctions(): array
     {
         return [
             new TwigFunction('render_table', [$this, 'renderTable'], [
