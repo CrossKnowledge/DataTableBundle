773f9c79dda1e3245c5cd4fceccf9250f506377a
Denis Makarenko <makarenkodeniz@gmail.com>
2025-10-10 15:01:45 +0200

    fix issues js

--- Resources/public/assets/js/ckdatable_ie8.js
+++ Resources/public/assets/js/ckdatable_ie8.js
@@ -4826,7 +4826,7 @@ function _classCallCheck(instance, Constructor) { if (!(instance instanceof Cons
                 if (customOptions != undefined) {
                     options = $.extend({}, options, customOptions);
                 }
-                this.table = this.element.find('table').DataTable(options);
+                this.table = new DataTable(this.element.querySelector('table'), options);
             }
         }]);
 
